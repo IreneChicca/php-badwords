6481dd45bde9a2d9a590a6a3835efdac55b42838
IreneChicca <irene.chicca@outlook.com>
2023-09-25 15:17:36 +0200

    Add form

---
+++ result.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+$testo = $_GET["text"]
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<h1> <?php $testo ?> </h1>
+<h1> <?php $bw ?> </h1>
+</body>
+</html>
